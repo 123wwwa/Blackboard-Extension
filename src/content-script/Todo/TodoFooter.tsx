214a47e96ed1c27837885e026828e531fb31868a
chan-hwi <gazebo5@naver.com>
2023-05-18 21:49:38 +0900

    css: change todo footer text input style

--- src/content-script/Todo/TodoFooter.tsx
+++ src/content-script/Todo/TodoFooter.tsx
@@ -22,13 +22,48 @@ const TodoFooterWrapper = styled.footer`
 	justify-content: space-between;
 	width: 100%;
 	padding: 10px 14px;
+	gap: 10px;
 
 	.menus {
+		width: 100%;
 		display: flex;
 		align-items: center;
 		gap: 5px;
 	}
 `;
+
+const styles = {
+	TextWrapper: css({
+		flex: 1,
+
+		"&::after": {
+			content: "''",
+			position: "absolute",
+			bottom: 0,
+			left: 0,
+			width: "100%",
+			height: "1px",
+			backgroundColor: "#434343",
+			transform: "scaleX(0)",
+			transition: "all 0.2s ease-in-out",
+		},
+
+		"&:focus-within::after": {
+			transform: "scaleX(1)",
+		},
+	}),
+	TextInput: css({
+		width: "100%",
+		border: "none",
+		borderBottom: "1px solid #dedede",
+		borderRadius: 0,
+		paddingLeft: 0,
+		position: "relative",
+	}),
+	DatePicker: css({
+		width: "fit-content",
+	}),
+};
 
 function TodoFooter({ color, setColor }: Props) {
 	const [currentDate, setCurrentDate] = React.useState<Date>(new Date());
@@ -36,9 +71,9 @@ function TodoFooter({ color, setColor }: Props) {
 	const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setTitle(e.target.value);
 	};
-	const dispatch = useDispatch(); 
+	const dispatch = useDispatch();
 	const onClickAdd = () => {
-		const todo:Todo = {
+		const todo: Todo = {
 			content: title,
 			color: color,
 			date: +currentDate,
@@ -47,16 +82,28 @@ function TodoFooter({ color, setColor }: Props) {
 		setTitle("");
 		setColor("#F5F5F5");
 		setCurrentDate(new Date());
-	}
+	};
 
 	return (
 		<TodoFooterWrapper>
 			<div className="menus">
 				<SketchColorPicker color={color} setColor={setColor} />
-				<TextInput placeholder="새 일정 이름 입력" value={title} onChange={onChangeTitle}/>
-				<DatePicker currentDate = {currentDate} setCurrentDate= {setCurrentDate}/>
+				<TextInput
+					placeholder="새 일정 이름 입력"
+					value={title}
+					onChange={onChangeTitle}
+					wrapperProps={{ css: styles.TextWrapper }}
+					css={styles.TextInput}
+				/>
+				<div>
+					<DatePicker
+						currentDate={currentDate}
+						setCurrentDate={setCurrentDate}
+						css={styles.DatePicker}
+					/>
+				</div>
 			</div>
-			<ActionIcon icon={faPlus} onClick={onClickAdd}/>
+			<ActionIcon icon={faPlus} onClick={onClickAdd} />
 		</TodoFooterWrapper>
 	);
 }
--- src/content-script/Todo/common/TextInput.tsx
+++ src/content-script/Todo/common/TextInput.tsx
@@ -1,33 +1,52 @@
-import React, { InputHTMLAttributes, ReactNode } from 'react'
-import styled from '@emotion/styled';
-import { css } from '@emotion/react';
+import React, { HTMLAttributes, InputHTMLAttributes, ReactNode } from "react";
+import styled from "@emotion/styled";
+import { css, Interpolation, Theme } from "@emotion/react";
 
 const InputContainer = styled.input<{ icon?: ReactNode }>`
-    border: 1px solid  #E9E9E9;
-    border-radius: 5px;
-    padding: 4px 9px;
-    font-size: 13px;
-    font-weight: 400;
-    ${props => props.icon && "padding-Right: 10px;"}
-    
-    &:focus {
-        outline: none;
-    }
+	border: 1px solid #e9e9e9;
+	border-radius: 5px;
+	padding: 4px 9px;
+	font-size: 13px;
+	font-weight: 400;
+	${(props) => props.icon && "padding-Right: 10px;"}
 
-    &::placeholder {
-      color: rgba(0, 0, 0, 0.4);
-    }
+	&:focus {
+		outline: none;
+	}
+
+	&::placeholder {
+		color: rgba(0, 0, 0, 0.4);
+	}
 `;
 
-type Props = InputHTMLAttributes<HTMLInputElement> & { icon?: ReactNode };
+type Props = InputHTMLAttributes<HTMLInputElement> &
+	React.ClassAttributes<HTMLInputElement> & {
+		icon?: ReactNode;
+		wrapperProps?: HTMLAttributes<HTMLDivElement> &
+			React.ClassAttributes<HTMLDivElement>;
+	};
 
-function TextInput({ ...props }: Props) {
-  return (
-    <div css={css({ position: "relative" })}>
-      <InputContainer icon={props.icon} {...props} />
-      {props.icon && <div css={css({ position: 'absolute', top: "50%", right: "4px", transform: "translateY(-50%)" })}>{props.icon}</div>}
-    </div>
-  );
+function TextInput({ wrapperProps, ...props }: Props) {
+	return (
+		<div css={[css({ position: "relative" }), wrapperProps?.css]}>
+			<InputContainer icon={props.icon} {...props} />
+			{props.icon && (
+				<div
+					css={[
+						css({
+							position: "absolute",
+							top: "50%",
+							right: "4px",
+							transform: "translateY(-50%)",
+						}),
+						props?.css,
+					]}
+				>
+					{props.icon}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default TextInput;
