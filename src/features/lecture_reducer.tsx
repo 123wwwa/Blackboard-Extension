08bf45efee6f302b54740f91a42f7035703e1827
123wwwa <123wwa@naver.com>
2023-08-22 23:33:41 +0900

    bug fix

--- src/features/lecture_reducer.tsx
+++ src/features/lecture_reducer.tsx
@@ -28,7 +28,7 @@ let initialState: InitialState = {
     bb_alarmList: [],
     alignWith: "date",
     checkedFiles: [],
-}
+};
 
 export const lectureSlice = createSlice({
     name: "lectureSlice",
@@ -64,12 +64,13 @@ export const lectureSlice = createSlice({
             state.alignWith = action.payload;
         },
         addCheckedFile: (state, action) => {
-           if(state.checkedFiles.find((file) => file.fileURL == action.payload.fileURL) == undefined){ // check if file is already checked
+            if (state.checkedFiles.find((file) => file.fileURL == action.payload.fileURL) == undefined) {
+                // check if file is already checked
                 state.checkedFiles.push(action.payload);
             }
         },
         removeCheckedFile: (state, action) => {
-            state.checkedFiles = state.checkedFiles.filter((file) => file.fileURL != action.payload.fileURL);   
+            state.checkedFiles = state.checkedFiles.filter((file) => file.fileURL != action.payload.fileURL);
         },
         setCheckedFiles: (state, action) => {
             state.checkedFiles = action.payload;
@@ -80,35 +81,54 @@ export const lectureSlice = createSlice({
                 Object.entries(state.lectureSlice).forEach(([key, value]) => {
                     let assignments = value.assignment;
                     assignments.forEach((assignment, index) => {
-                        state.lectureSlice[key].assignment[index].Assignment_Files = assignment.Assignment_Files.filter((fileUrl) => fileUrl.fileURL != file.fileURL);
-                        state.lectureSlice[key].assignment[index].fileUrl = assignment.fileUrl.filter((fileUrl) => fileUrl.fileURL != file.fileURL);
+                        state.lectureSlice[key].assignment[index].Assignment_Files = assignment.Assignment_Files.filter(
+                            (fileUrl) => fileUrl.fileURL != file.fileURL
+                        );
+                        state.lectureSlice[key].assignment[index].fileUrl = assignment.fileUrl.filter(
+                            (fileUrl) => fileUrl.fileURL != file.fileURL
+                        );
                     });
-                });  
-            })
-        }
+                });
+            });
+        },
     },
 });
-export const { setLecutureList, setShapedLectureList, setLectureAssignment, setTodoList, addTodo, addDeletedTodo, resetDeletedTodo, setBB_alarms, setAlignWith, addCheckedFile, setCheckedFiles, removeCheckedFile, deleteSelectedFiles} = lectureSlice.actions;
+export const {
+    setLecutureList,
+    setShapedLectureList,
+    setLectureAssignment,
+    setTodoList,
+    addTodo,
+    addDeletedTodo,
+    resetDeletedTodo,
+    setBB_alarms,
+    setAlignWith,
+    addCheckedFile,
+    setCheckedFiles,
+    removeCheckedFile,
+    deleteSelectedFiles,
+} = lectureSlice.actions;
 
 export const getLectureList = async (dispatch: AppDispatch) => {
     let lectureInfoStr = await getChromeStorage("lectureInfo", "{}");
     let resLecturelist: LectureList = JSON.parse(lectureInfoStr);
     //let assignmentListStr = await getChromeStorage("fileInfo", "{}");
-    let assignmentList:AssignmentList  = JSON.parse(localStorage.getItem('fileInfo') || "{}");
+    let assignmentList: AssignmentList = JSON.parse(localStorage.getItem("fileInfo") || "{}");
     //check if fileinfo is empty
-    if(Object.keys(assignmentList).length == 0){
+    if (Object.keys(assignmentList).length == 0) {
         console.error("fileInfo is empty");
-        return;
+    } else {
+        Object.entries(assignmentList).forEach(([key1, value1]) => {
+            let course_id: string = key1.split("-")[1];
+            Object.entries(resLecturelist).forEach(([key2, value]) => {
+                let lecture: any = value;
+                if (lecture.id == course_id) {
+                    resLecturelist[key2].assignment.push(value1 as Assignment);
+                }
+            });
+        });
     }
-    Object.entries(assignmentList).forEach(([key1, value1]) => {
-        let course_id: string = key1.split("-")[1];
-        Object.entries(resLecturelist).forEach(([key2, value]) => {
-            let lecture: any = value;
-            if (lecture.id == course_id) {
-                resLecturelist[key2].assignment.push(value1 as Assignment);
-            }
-        })
-    })
+
     dispatch(setLecutureList(resLecturelist));
     let l: ShapedLecture[][] = [[], [], [], [], []];
     let i = 0;
@@ -119,31 +139,29 @@ export const getLectureList = async (dispatch: AppDispatch) => {
         for (let c = 0; c < 3; c++) {
             if (item["timeplace" + c]) {
                 let newItem: ShapedLecture = {
-                    "name": item["name"],
-                    "professor": item["professor"],
-                    "time": item["time"],
-                    "link": item["link"],
-                    "color": item["color"],
-                    "timeplace": item["timeplace" + c]
-                }
+                    name: item["name"],
+                    professor: item["professor"],
+                    time: item["time"],
+                    link: item["link"],
+                    color: item["color"],
+                    timeplace: item["timeplace" + c],
+                };
                 l[item["timeplace" + c].day].push(newItem);
-
             }
         }
     }
     dispatch(setShapedLectureList(l));
-
-}
+};
 export const getTodoList = async (dispatch: AppDispatch) => {
     let todoListStr = await getChromeStorage("todoList", "[]");
     let todoList: Todo[] = JSON.parse(todoListStr);
     dispatch(setTodoList(todoList));
     //postTodoList(todoList);
-}
+};
 export const resetTodoList = async (dispatch: AppDispatch) => {
     setChromeStorage("deletedTodoList", "[]");
     dispatch(reloadTodoList);
-}
+};
 export const reloadTodoList = async (dispatch: AppDispatch) => {
     let todoListStr = await getChromeStorage("todoList", "[]");
     let todoList: Todo[] = JSON.parse(todoListStr);
@@ -154,17 +172,20 @@ export const reloadTodoList = async (dispatch: AppDispatch) => {
             todoList.splice(parseInt(key), 1);
         }
     }
-    const fetchUrl = "https://blackboard.unist.ac.kr/webapps/calendar/calendarData/selectedCalendarEvents?start=" + Date.now() + "&end=2147483647000";
+    const fetchUrl =
+        "https://blackboard.unist.ac.kr/webapps/calendar/calendarData/selectedCalendarEvents?start=" +
+        Date.now() +
+        "&end=2147483647000";
     const fetchData = await APIwithcatch(fetchUrl, "{}");
     if (!fetchData) {
         dispatch(getTodoList);
         return;
-    };
-    
+    }
+
     if (fetchData.length == 0) {
         dispatch(getTodoList);
         return;
-    };
+    }
     let resLecturelistStr = await getChromeStorage("lectureInfo", "{}");
     let resLecturelist: LectureList = JSON.parse(resLecturelistStr);
     // remove deleted todo
@@ -185,7 +206,7 @@ export const reloadTodoList = async (dispatch: AppDispatch) => {
             if (lecture.engName == fetchData[key]["calendarName"]) {
                 letureColor = lecture.color;
             }
-        })
+        });
         let newStartString = fetchData[key]["start"];
         let newDate = new Date(newStartString);
         let assignName = fetchData[key]["title"];
@@ -199,7 +220,7 @@ export const reloadTodoList = async (dispatch: AppDispatch) => {
             content: assignName,
             date: newDate.getTime(),
             color: letureColor,
-            linkcode: link
+            linkcode: link,
         };
         todoList.push(todo);
     }
@@ -221,9 +242,10 @@ export const reloadTodoList = async (dispatch: AppDispatch) => {
     setChromeStorage("todoList", JSON.stringify(newTodoList));
     dispatch(setTodoList(newTodoList));
     //postTodoList(newTodoList);
-}
+};
 export const deleteTodo = (dispatch: AppDispatch) => async (todo: Todo) => {
-    if (todo.linkcode) { // check if linkcode exist to add only fetched todo
+    if (todo.linkcode) {
+        // check if linkcode exist to add only fetched todo
         dispatch(addDeletedTodo(todo));
         let deletedTodoListStr = await getChromeStorage("deletedTodoList", "[]");
         let deletedTodoList: Todo[] = JSON.parse(deletedTodoListStr);
@@ -244,7 +266,7 @@ export const deleteTodo = (dispatch: AppDispatch) => async (todo: Todo) => {
     setChromeStorage("todoList", JSON.stringify(newTodoList));
     dispatch(setTodoList(newTodoList));
     //postTodoList(newTodoList);
-}
+};
 export const addTodoItem = (dispatch: AppDispatch) => async (todo: Todo) => {
     // check if duplicated
     let todoListStr = await getChromeStorage("todoList", "[]");
@@ -259,59 +281,60 @@ export const addTodoItem = (dispatch: AppDispatch) => async (todo: Todo) => {
     dispatch(addTodo(todo));
     todoList.push(todo);
     setChromeStorage("todoList", JSON.stringify(todoList));
-}
+};
 export const getBB_alarms = async (dispatch: AppDispatch) => {
     let bb_alarmsStr = await getChromeStorage("BB_alarms", "[]");
     let bb_alarms: BB_alarm[] = JSON.parse(bb_alarmsStr);
     console.log(bb_alarms);
     dispatch(setBB_alarms(bb_alarms));
-}
+};
 export const reloadBB_alarms = async (dispatch: AppDispatch) => {
     const url = "https://blackboard.unist.ac.kr/webapps/streamViewer/streamViewer";
     const fetchdata = await fetch(url, {
-        "headers": {
-          "accept": "text/javascript, text/html, application/xml, text/xml, */*",
-          "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-          "sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"macOS\"",
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
-          "x-prototype-version": "1.7",
-          "x-requested-with": "XMLHttpRequest"
+        headers: {
+            accept: "text/javascript, text/html, application/xml, text/xml, */*",
+            "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "sec-ch-ua": '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-prototype-version": "1.7",
+            "x-requested-with": "XMLHttpRequest",
         },
-        "referrer": "https://blackboard.unist.ac.kr/webapps/streamViewer/streamViewer?cmd=view&streamName=alerts&globalNavigation=false",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": "cmd=loadStream&streamName=alerts&providers=%7B%7D&forOverview=false",
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
-      });
-    if(!fetchdata.ok) {
+        referrer:
+            "https://blackboard.unist.ac.kr/webapps/streamViewer/streamViewer?cmd=view&streamName=alerts&globalNavigation=false",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: "cmd=loadStream&streamName=alerts&providers=%7B%7D&forOverview=false",
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+    });
+    if (!fetchdata.ok) {
         dispatch(getBB_alarms);
         return;
-    };
-    
+    }
+
     let alarmListStr = await fetchdata.text();
-    if(!alarmListStr) {
+    if (!alarmListStr) {
         dispatch(getBB_alarms);
         return;
-    };
+    }
     let rawAlarmList = JSON.parse(alarmListStr).sv_streamEntries;
-    if(rawAlarmList.length == 0) {
+    if (rawAlarmList.length == 0) {
         //dispatch(getBB_alarms);
         //console.log("no alarm");
         return;
-    };
-    let BB_alarms =  await convertBB_alarm(alarmListStr);
-    setChromeStorage("BB_alarms", JSON.stringify(BB_alarms));  
+    }
+    let BB_alarms = await convertBB_alarm(alarmListStr);
+    setChromeStorage("BB_alarms", JSON.stringify(BB_alarms));
     dispatch(setBB_alarms(BB_alarms));
-}
+};
 export const postTodoList = async (todoList: Todo[]) => {
     window.chrome.runtime.sendMessage({ action: "updateTodo", todoList: todoList });
-}
+};
 export const selectLectureList = (state: RootState) => state.lectureSlice.lectureSlice;
 export const selectShapedLectureList = (state: RootState) => state.lectureSlice.shapedLectureList;
 export const selectIsLectureLoaded = (state: RootState) => state.lectureSlice.isLectureLoaded;
